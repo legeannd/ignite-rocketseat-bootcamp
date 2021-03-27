96014823beda85a5469e1f5a21b3cbcfe8cf1304
Gean Lucas <gean.lfpsa@gmail.com>
2021-03-27 17:22:39 -0300

    Inserting transactions in transactions table after transaction modal being called

--- reactjs/chapter-2/dtmoney/src/components/NewTransactionModal/index.tsx
+++ reactjs/chapter-2/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,7 +1,6 @@
 import { FormEvent, useState, useContext } from 'react';
 import Modal from 'react-modal';
 import { Container, RadioBox, TransactionTypeContainer } from './styles';
-import { api } from '../../services/api';
 import { TransactionsContext } from '../../TransactionsContext';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
@@ -22,15 +21,21 @@ export function NewTransactionModal({isOpen, onRequestClose}: NewTransactionModa
   const [category, setCategory] = useState('');
   const [type, setType] = useState('deposit');
 
-  function handleCreateNewTransaction(event: FormEvent) {
+  async function handleCreateNewTransaction(event: FormEvent) {
     event.preventDefault();
 
-    createTransaction({
+    await createTransaction({
       title, 
       amount, 
       category,
       type
     });
+
+    setTitle('');
+    setAmount(0);
+    setCategory('');
+    setType('deposit');
+    onRequestClose();
   }
   
   return (
