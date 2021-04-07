bba35946b5280708ee564851fccf20098250edde
Gean Lucas <gean.lfpsa@gmail.com>
2021-04-06 22:17:41 -0300

    Enabling static site generation to optimize homepage accesses

--- reactjs/chapter-3/ignews/src/pages/index.tsx
+++ reactjs/chapter-3/ignews/src/pages/index.tsx
@@ -1,9 +1,13 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { SubscribeButton } from '../components/SubscribeButton';
 import { stripe } from '../services/stripe';
 
 import styles from './home.module.scss';
+
+// Client-side rendering
+// Server-side rendering
+// Static Site Generation
 
 interface HomeProps {
   product: {
@@ -36,7 +40,7 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1IdPIJLXSYAnShEJ44aCjF3E');
 
   const product = {
@@ -50,6 +54,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return { 
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
