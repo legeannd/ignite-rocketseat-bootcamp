ab64003dacb20844e56324a5417477df9da770b5
Gean Lucas <gean.lfpsa@gmail.com>
2021-05-17 23:03:40 -0300

    Changing header to make it responsive

--- reactjs/chapter-4/dashgo/src/components/Header/Profile.tsx
+++ reactjs/chapter-4/dashgo/src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Gean Lucas</Text>
-        <Text color="gray.300" fontSize="small">
-          gean.lfpsa@gmail.com
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Gean Lucas</Text>
+          <Text color="gray.300" fontSize="small">
+            gean.lfpsa@gmail.com
         </Text>
-      </Box>
+        </Box>
+      )}
 
       <Avatar size="md" name="Gean Lucas" src="https://github.com/legeannd.png" />
     </Flex>
